62acd5e3fc3e51f4d5e8f2e53b6a34476d162c52
Omar <90123670+omarnunezsiri@users.noreply.github.com>
2026-06-27 16:49:37 -0400

    feat(terminal-utils): add git aliases and utilities

--- PowerShell/TerminalUtilities.ps1
+++ PowerShell/TerminalUtilities.ps1
@@ -1,4 +1,4 @@
-Function CommandExists {
+function CommandExists {
     Param ($command)
     $preference = $ErrorActionPreference
     $ErrorActionPreference = 'SilentlyContinue'
@@ -6,6 +6,15 @@ Function CommandExists {
     Catch { Write-Host "$command does not exist"; RETURN $false }
     Finally { $ErrorActionPreference = $preference }
 }
+
+
+# Unix-like sudo
+function sudo { 
+    Start-Process wt -Verb runAs
+}
+
+# Open the current directory in the file explorer
+function here() { Invoke-Item . }
 
 # Unix-like which command
 function which($name) { Get-Command $name | Select-Object -ExpandProperty Definition }
@@ -22,3 +31,36 @@ function rmh() { Remove-Item (Get-PSReadlineOption).HistorySavePath }
 # Compute file hashes
 function sha1 { Get-FileHash -Algorithm SHA1 $args }
 function sha256 { Get-FileHash -Algorithm SHA256 $args }
+
+#region git
+
+### Git Aliases/Utilities
+function ginit() {
+    git init
+    git add .
+    git commit -m "feat: initialize repository"
+}
+
+function glog() {
+    git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative
+}
+
+function gd() { git diff --color }
+
+function lazycom() {
+    git add .
+    git commit -m "$args"
+    gpsh
+}
+
+function gst() { git status -sb }
+
+function gpsh() { git push origin HEAD }
+
+function gpl() { git pull --prune }
+
+function gco() {
+    git checkout $args
+    Write-Host "Switched to branch: " -NoNewline; Write-Host $args -ForegroundColor Cyan
+}
+#endregion
